672ab0f68c58493758c82d5c764c8fa0c7ab93d4
jay-shi <j67shi@edu.uwaterloo.ca>
2017-11-05 00:34:13 -0400

    update slave

--- Slave_Servo_Working/Slave_Servo_Working.ino
+++ Slave_Servo_Working/Slave_Servo_Working.ino
@@ -1,22 +1,23 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
+#define VoltageSensor A0
+#define ESCConteroller 9
+
 SoftwareSerial BTserial(11,10);
 Servo ESC; 
 
-static const voltageSensor A0,
-        ESCConteroller 9;
 int servoSpeed = 90; //rest position of motor
     curVal = 90, //current value of the potentiomter
-    brakeSpeed = 1, 
-    buttonTerminate = 0; 
+    brakeSpeed = 1,
+    currentSpeed=0;
 
 /*
- * check and send battery level
+ * check and send battery level to the master arduino
  */
 void function sendBatteryLevel(){
-  int voltageSensorValue = analogRead(voltageSensor);
-  float voltage = voltageSensorValue * (5.0/1023.0); 
+  int voltageSensorValue = analogRead(VoltageSensor);
+  float voltage = map(voltageSensorValue, 0, 1023.0, 0.0, 5.0); 
   if(voltage > 4.25){
     BTserial.write(3); 
     return;
@@ -25,73 +26,72 @@ void function sendBatteryLevel(){
     BTserial.write(2); 
     return;
   }
-
   BTserial.write(1);
   return;
 }
 
-void function accelerate(){
-  // implement functions that accelerate esc
-  ESC.write();
-  ESC.writeMicroseconds(2000); // control the duration of one pulse
+/**
+ * send mappedSpeedValue to ESC
+*/
+void function accelerate(int mappedSpeedValue){
+  while(currentSpeed < mappedSpeedValue){
+    currentSpeed += 1;
+    ESC.write(currentSpeed);
+    delay(1000);
+  }
+  return; 
 }
 
-void function deaccelerate(){
-  // implement functions that deaccelerate esc
+/**
+ * send mappedSpeedValue to ESC
+*/
+void function deaccelerate(int mappedSpeedValue){
+  while(currentSpeed > mappedSpeedValue){
+    currentSpeed -= 1;
+    ESC.write(currentSpeed);
+    delay(1000);
+  }
+  return; 
 }
 
-void function defaultBrake(){
-
-}
-
-void function cruiseMode(){
-
+/**
+ * stop the longboard. brake at a constant deacceleration value
+*/
+void function stop(){
+  while(currentSpeed >= 0){
+    currentSpeed -= 1;
+    ESC.write(currentSpeed);
+    delay(1000);
+  }
+  return; 
 }
 
 void setup() {
   Serial.begin(9600); // Sets the data rate in bits per second (baud) for serial data transmission
   BTserial.begin(38400); // Default communication rate of the Bluetooth module
-  ESC.attach(ESCConteroller); 
+  ESC.attach(ESCConteroller); // connect to ESC
 }
 
 void loop() {
+
   if(BTserial.available()>0){
-    while(curVal < servoSpeed && buttonTerminate == 0){
-      servoSpeed = BTserial.read();
-      curVal = curVal+1;
-      ESC.write(curVal); 
-      delay(50);
-//    Serial.println(curVal);
-    }
-
-    while(curVal > servoSpeed && servoSpeed < 81 && ttonTerminate == 0){
-      
-        servoSpeed = BTserial.read();
-//          Serial.println(servoSpeed); 
-//          Serial.println(curVal);
-          if(servoSpeed > -1){
-            brakeSpeed = -(9.0/79.0)*servoSpeed + 10.1139241;
-          }else if(servoSpeed == -10){
-              brakeSpeed = 5; 
-              while(curVal > 82){
-                  curVal = curVal - brakeSpeed;
-                }
-              buttonTerminate = 1; 
-          }
-          curVal = curVal - brakeSpeed;
-          if (curVal < 82){ 
-            curVal = 82;
-          }
-          ESC.write(curVal);
-          delay(50); 
-    }
-//    Serial.println(curVal);
-    servoSpeed = BTserial.read();
-
+      int receivedValue = BTserial.read();
   }
 
+  if(receivedValue>currentSpeed){
+    accelerate(receivedValue);
+    return;
+  }
+
+  if(receivedValue< currentSpeed && receivedValue> 0 ){
+    deaccelerate(receivedValue);
+    return;
+  }
+
+  if(receivedValue == 0){
+    stop();
+    return;
+  }
   sendBatteryLevel();
-  delay(50);
-  
 }
 
